99744f1ee5aa39ff5aae9ed3fa93b1603007c26b
Support <support@subpointsolutions.com>
2016-10-15 20:05:49 +1100

    + build and tests fixes for CI

--- MetaPack.Tests/Scenarios/ClientScenarioTests.cs
+++ MetaPack.Tests/Scenarios/ClientScenarioTests.cs
@@ -31,7 +31,7 @@ namespace MetaPack.Tests.Scenarios
 
         [TestMethod]
         [TestCategory("Metapack.Client.Commands")]
-        [TestCategory("CI.Core")]
+        //[TestCategory("CI.Core")]
         public void Can_Call_List_Command()
         {
             var webSiteUrl = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.RootWebUrl);
@@ -47,6 +47,28 @@ namespace MetaPack.Tests.Scenarios
         [TestMethod]
         [TestCategory("Metapack.Client.Commands")]
         [TestCategory("CI.Core")]
+        public void Can_Call_List_Command_O365()
+        {
+            var webSiteUrl = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.RootWebUrl);
+            var userName = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.O365UserName);
+            var userPassword = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.O365UserPassword);
+
+            var command = new NuGetListCommand
+            {
+                Url = webSiteUrl,
+
+                UserName = userName,
+                UserPassword = userPassword,
+
+                IsSharePointOnline = true
+            };
+
+            command.Execute();
+        }
+
+        [TestMethod]
+        [TestCategory("Metapack.Client.Commands")]
+        //[TestCategory("CI.Core")]
         public void Can_Call_Install_Command()
         {
             var webSiteUrl = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.RootWebUrl);
@@ -81,6 +103,62 @@ namespace MetaPack.Tests.Scenarios
                     Version = packageVersion,
 
                     PreRelease = true
+                };
+
+                command.Execute();
+
+                hasInstallHit = true;
+            });
+
+
+
+        }
+
+        [TestMethod]
+        [TestCategory("Metapack.Client.Commands")]
+        [TestCategory("CI.Core")]
+        public void Can_Call_Install_Command_O365()
+        {
+            var webSiteUrl = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.RootWebUrl);
+            var userName = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.O365UserName);
+            var userPassword = EnvironmentUtils.GetEnvironmentVariable(RegConsts.SharePoint.O365UserPassword);
+
+            var hasInstallHit = false;
+
+            var solutionPackage = CreateNewSolutionPackage(SolutionPackageType.SPMeta2);
+            UpdatePackageVersion(solutionPackage);
+
+            var packageVersion = solutionPackage.Version;
+            var packageId = solutionPackage.Id;
+
+            var packagingService = new SPMeta2SolutionPackageService();
+
+            WithNuGetContext((apiUrl, apiKey, repoUrl) =>
+            {
+                // push a new version of the package
+                packagingService.Push(solutionPackage, apiUrl, apiKey);
+
+                // get the package
+                var ciRepo = PackageRepositoryFactory.Default.CreateRepository(repoUrl);
+                var ciPackage = ciRepo.FindPackageSafe(packageId, new SemanticVersion(packageVersion));
+
+                Assert.IsNotNull(ciPackage, "Solution package");
+
+                // command testing
+                var command = new DefaultInstallCommand
+                {
+                    Source = repoUrl,
+                    Url = webSiteUrl,
+
+                    Id = packageId,
+                    Version = packageVersion,
+
+                    PreRelease = true,
+
+                    UserName = userName,
+                    UserPassword = userPassword,
+
+                    IsSharePointOnline = true
                 };
 
                 command.Execute();
